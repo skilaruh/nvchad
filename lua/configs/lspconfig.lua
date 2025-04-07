251dc49f9f3e44177e322c99d0f91191270fd7e0
skilaruh <85468370+skilaruh@users.noreply.github.com>
2025-04-06 22:37:04 -0400

    Removed zls and rust_analyzer from lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require("lspconfig")
 
 -- EXAMPLE
-local servers = { "clangd", "gopls", "r_language_server", "ruff", "rust_analyzer", "zls" }
+local servers = { "clangd", "gopls", "r_language_server", "ruff" }
 local nvlsp = require("nvchad.configs.lspconfig")
 
 -- lsps with default config
