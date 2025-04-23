67aca3a85628035d46892afb1db1886f25a7ef5c
skilaruh <85468370+skilaruh@users.noreply.github.com>
2025-04-23 18:55:54 -0400

    Removed coderunner & updated packages

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -21,10 +21,4 @@ return {
         "folke/which-key.nvim",
         lazy = false,
     },
-
-    {
-        "CRAG666/code_runner.nvim",
-        opts = require("configs.code-runner"),
-        lazy = false,
-    },
 }
