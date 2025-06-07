bbcbc03388cce8aabedd11d1fbfafb61a770c2a6
skilaruh <85468370+skilaruh@users.noreply.github.com>
2025-06-07 17:02:06 -0400

    Added a custom code runner (code_runner.lua)

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,3 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: Code Action" })
+
+map("n", "<leader>r", function()
+    require("configs.code_runner").run()
+end, { desc = "Run current file in horizontal terminal" })
