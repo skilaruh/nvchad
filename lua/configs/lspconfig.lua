db7c331ec0bda84fd460da968c70befc374d8743
Raunak Chattopadhyay <skilaruh@bruhbook-pro.local>
2025-06-07 01:23:03 -0400

    Removed r language server from lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require("lspconfig")
 
 -- EXAMPLE
-local servers = { "clangd", "gopls", "r_language_server", "ruff" }
+local servers = { "clangd", "gopls", "ruff" }
 local nvlsp = require("nvchad.configs.lspconfig")
 
 -- lsps with default config
